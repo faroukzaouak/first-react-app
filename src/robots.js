9a13377c3614a104902aa61584aee19ffb37d8f7
faroukzaouak <omar.zaouak@univ-constantine2.dz>
2025-02-11 18:01:26 +0100

    first commit

---
+++ src/robots.js
@@ -0,0 +1,51 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: '1w7Vt3@example.com'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Oa0Ct@example.com'
+    },
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'w7Vt3@example.com'
+    },
+    {
+        id: 4,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'w7Vt3@example.com'
+    },
+    {
+        id: 5,
+        name: 'Chelsey Dietrich',
+        username: 'Kamren',
+        email: 'w7Vt3@example.com'
+    },{
+        id: 6,
+        name: 'Mrs. Dennis Schulist',
+        username: 'Leopoldo_Corkery',
+        email: 'w7Vt3@example.com'
+    },{
+        id: 7,
+        name: 'Kurtis Weissnat',
+        username: 'Elwyn.Skiles',
+        email: 'w7Vt3@example.com'
+    },{
+        id: 8,
+        name: 'Nicholas Runolfsdottir V',
+        username: 'Maxime_Nienow',
+        email: 'w7Vt3@example.com'
+    },{
+        id: 9,
+        name: 'Glenna Reichert',
+        username: 'Delphine',
+        email: 'w7Vt3@example.com'
+    }]

9541b62a62e11509ea6c8e1f4dd4e7a7530ac8e4
faroukzaouak <omar.zaouak@univ-constantine2.dz>
2025-02-13 10:06:53 +0100

    adding loading animation and json data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Cardlist from './Cardlist';
-import { robots } from './robots';
 import Searchbox from './Searchbox';
 import './app.css';
 
@@ -8,33 +7,45 @@ class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
-    searchchange=(event)=>{
-        
-        this.setState({searchfield: event.target.value});
+    searchchange = (event) => {
+
+        this.setState({ searchfield: event.target.value });
+
+    }
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }));
         
     }
 
 
     render() {
         const filteredrobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase()) 
-            || robot.email.toLowerCase().includes(this.state.searchfield.toLowerCase()) 
-            || robot.id.toString().includes(this.state.searchfield.toLowerCase());
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+                || robot.email.toLowerCase().includes(this.state.searchfield.toLowerCase())
+                || robot.id.toString().includes(this.state.searchfield.toLowerCase());
         });
-        return (
-            <div className="tc">
-                
-                <h1>RoboFriends</h1>
-                <Searchbox searchchange={this.searchchange} />
-                <Cardlist robots={filteredrobots} />
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return <h1 className='tc loading-animation'>Loading...</h1>;
+        }
+        
+        else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <Searchbox searchchange={this.searchchange} />
+                    <Cardlist robots={filteredrobots} />
+                </div>
+            );
+        }
+        
     }
-    
+
 }
 
 export default App;
